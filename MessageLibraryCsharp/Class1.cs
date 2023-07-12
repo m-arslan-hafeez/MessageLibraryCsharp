b7e395f84dec9b010e91063767fd81c83facec70
OpenAI <OpenAI@CHATGPT>
2023-07-12 10:07:14 +0500

    Add project files.

---
+++ MessageLibraryCsharp/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MessageBoxDLL
+{
+    public class MessageBoxDLL
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+        public static void ShowMessage(string message, string caption)
+        {
+            MessageBox(IntPtr.Zero, message, caption, 0);
+        }
+    }
+}
